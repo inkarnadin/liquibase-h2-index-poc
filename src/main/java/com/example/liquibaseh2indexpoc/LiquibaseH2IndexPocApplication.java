c40d3ad5a91086da46212ae503b103fb2c03fc52
inkarnadin <nasyrovvad@gmail.com>
2022-03-17 02:40:50 +0300

    build: init commit

---
+++ src/main/java/com/example/liquibaseh2indexpoc/LiquibaseH2IndexPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibaseh2indexpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseH2IndexPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseH2IndexPocApplication.class, args);
+    }
+
+}
